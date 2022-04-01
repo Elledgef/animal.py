95b51cd277b1a287b9ab14b1f27f65dd2c720581
faith elledge <faithelledge@faiths-MacBook-Air.local>
2022-03-31 17:58:30 -0700

    Initial commit

---
+++ animal.py
@@ -0,0 +1,8 @@
+#Author: Faith Elledge
+#Github Username: Elledgef
+#Date: 3/31/2021
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
